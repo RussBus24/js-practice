b89932c96dcb4c19a6ad2c7984110156fc1cb8f9
Russell Henry <russbus24@Russells-MacBook-Pro.local>
2017-03-15 19:36:18 -0400

    More practice files

---
+++ conversion.js
@@ -0,0 +1,9 @@
+
+var a = 33;
+console.log(a.toString(2));
+
+var b = 8;
+console.log(b.toString(2));
+
+var c = a | b;
+console.log(c.toString(2));
---
+++ progress.js
@@ -0,0 +1,19 @@
+function onStart() {
+    for (var i = 0; i < 11; i++) {
+        console.log(i);
+    }
+}
+
+function onProgress(seconds, callback) {
+
+}
+
+function onEnd(seconds, callback) {
+
+}
+
+function wait(seconds, callback) {
+    setTimeout()
+}
+
+onStart();
